3e9c5114e8542ca264531a1e5ea61bbf12dd3312
Smitty971 <jlsmitty2273@yahoo.com>
2021-01-27 17:18:59 -0500

    added to migrations

--- rails-backend/db/migrate/20210117010749_create_posts.rb
+++ rails-backend/db/migrate/20210117010749_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :description
+      t.string :artist
+      t.references :user
       t.timestamps
     end
   end
