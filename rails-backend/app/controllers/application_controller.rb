a0e36eedcf2fb8f75a91490eb5653f83ac975a89
Smitty971 <jlsmitty2273@yahoo.com>
2021-01-31 18:08:10 -0500

    added some helpers  folders

--- rails-backend/app/controllers/application_controller.rb
+++ rails-backend/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    before_action :current_user, only: [:new, :edit, :delete, :create]
+    before_action :require_login, only: [:new, :edit, :delete, :index]
+
+    
 end
