0270a82f8506123ca17fbd91186d1e5d1a9c4d57
Smitty971 <jlsmitty2273@yahoo.com>
2021-01-31 17:47:35 -0500

    coded user model

--- rails-backend/app/models/user.rb
+++ rails-backend/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    has_many :comments, through: :posts
+    has_many :posts
+    validates_presence_of :username
+    validates_presence_of :email, unless :github_uid?
+
+    def github_uid?
+        github_uid != nil
+    end
+
+    # if github_uid == nil
+    #     validates_presence_of :email
+    # end
+        
+    has_secure_password
 end
