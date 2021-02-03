63797138326ab081de29ac409ea99758b1165c2d
Smitty971 <jlsmitty2273@yahoo.com>
2021-02-03 18:31:40 -0500

    removed incorrect code

--- rails-backend/app/controllers/application_controller.rb
+++ rails-backend/app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::API
-    before_action :current_user, only: [:new, :edit, :delete, :create]
-    before_action :require_login, only: [:new, :edit, :delete, :index]
-
-    
 end
--- rails-backend/app/models/comment.rb
+++ rails-backend/app/models/comment.rb
@@ -1,5 +1,3 @@
 class Comment < ApplicationRecord
-    belongs_to :posts
-    validates :username, presence: true
-    validate :description, presence: true
+
 end
