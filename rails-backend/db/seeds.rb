f0014022d310cf59110c5c0328413f4d4ce2430d
Smitty971 <jlsmitty2273@yahoo.com>
2021-02-04 18:48:24 -0500

    added seeds for test

--- rails-backend/db/seeds.rb
+++ rails-backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create(artist: "jim")
+Post.create(artist: "jan")
+Post.create(artist: "kimmmmm")
+Post.create(artist: "yekkkka")
