7395dec6191e31c7e6cfcc05571087537470c0ad
Smitty971 <jlsmitty2273@yahoo.com>
2021-02-21 20:49:47 -0500

    added cors functionality

--- js-frontend/src/app.js
+++ js-frontend/src/app.js
@@ -6,11 +6,12 @@ class App {
 
     getPosts() {
         // make a fetch request to /posts
-        fetch(url + '/posts')
+        console.log('something')
+        fetch(this.url + '/posts')
         .then(resp => console.log(resp))
         //populate the posts and comments with the returned data
         //call renderPosts
-
+        .catch(err => alert(err))
     }
 
     renderPosts() {
